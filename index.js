7c04b5190e9fa0f187c6241cb03c0c156a281359
Jana Abfalterer <abfalterer.jana@gmail.com>
2022-05-03 18:33:24 +0200

    Fix Problems with packages

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 import chalk from 'chalk';
 import randomColor from 'randomcolor';
 
-let color = randomColor({
+const color = randomColor({
   hue: process.argv[2],
   luminosity: process.argv[3],
 });
 
-let colorHue = randomColor({
+const colorHue = randomColor({
   luminosity: process.argv[2],
 });
 
@@ -22,8 +22,8 @@ const outputString = (c) => {
 ###############################`;
 };
 
-if (process.argv[2] !== 'light' || 'dark') {
-  console.log(chalk.hex(color)(outputString(color)));
-} else {
+if (process.argv[2] === 'light' || process.argv[2] === 'dark') {
   console.log(chalk.hex(colorHue)(outputString(colorHue)));
+} else {
+  console.log(chalk.hex(color)(outputString(color)));
 }
