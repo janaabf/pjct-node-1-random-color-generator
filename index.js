e1ed60faa2bc70f5376739284bb50623abbc8ff4
Jana Abfalterer <abfalterer.jana@gmail.com>
2022-05-04 11:46:40 +0200

    Add comments & optimize index.js

--- index.js
+++ index.js
@@ -1,29 +1,34 @@
-import chalk from 'chalk';
-import randomColor from 'randomcolor';
+import chalk from 'chalk'; //colors text in terminal
+import randomColor from 'randomcolor'; //generates random colors & lets us define some values
 
 const color = randomColor({
   hue: process.argv[2],
   luminosity: process.argv[3],
 });
 
-const colorHue = randomColor({
+const colorLum = randomColor({
   luminosity: process.argv[2],
+  hue: process.argv[3],
 });
 
-const outputString = (c) => {
+//function to output the desired block. Input is the random generated color:
+const outputString = (color) => {
   return `###############################
 ###############################
 ###############################
 #####                     #####
-#####        ${c}      #####
+#####        ${color}      #####
 #####                     #####
 ###############################
 ###############################
 ###############################`;
 };
 
+/* chooses between const color & colorLum.
+color accepts hue first, the luminosity second.
+colorLum accepts the luminosity first, hue second. */
 if (process.argv[2] === 'light' || process.argv[2] === 'dark') {
-  console.log(chalk.hex(colorHue)(outputString(colorHue)));
+  console.log(chalk.hex(colorLum)(outputString(colorLum)));
 } else {
   console.log(chalk.hex(color)(outputString(color)));
 }
