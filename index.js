0f5c25fb26cb2ed0dc8ef7bf2fe5b847c68af1dc
Jana Abfalterer <abfalterer.jana@gmail.com>
2022-06-28 11:08:26 +0200

    Update variable names & adjust comments

--- index.js
+++ index.js
@@ -1,34 +1,36 @@
 import chalk from 'chalk'; // colors text in terminal
 import randomColor from 'randomcolor'; // generates random colors & lets us define some values
 
-const color = randomColor({
+// process.argv[i] takes whatever word is written in console in the position of i
+const colorHueFirst = randomColor({
   hue: process.argv[2],
   luminosity: process.argv[3],
 });
 
-const colorLum = randomColor({
+const colorLuminosityFirst = randomColor({
   luminosity: process.argv[2],
   hue: process.argv[3],
 });
 
-// function to output the desired block. Input is the random generated color:
-const outputString = (hex) => {
+// function to output the desired block. Input is the randomly generated color (format: hex code):
+const outputString = (hexColorCode) => {
   return `###############################
 ###############################
 ###############################
 #####                     #####
-#####        ${hex}      #####
+#####        ${hexColorCode}      #####
 #####                     #####
 ###############################
 ###############################
 ###############################`;
 };
 
-/* chooses between const color & colorLum.
-color accepts hue first, the luminosity second.
-colorLum accepts the luminosity first, hue second. */
+// selects code (colorHueFirst or ColorLuminosityFirst)
+// based on wether user specifies color or hue first
 if (process.argv[2] === 'light' || process.argv[2] === 'dark') {
-  console.log(chalk.hex(colorLum)(outputString(colorLum)));
+  console.log(
+    chalk.hex(colorLuminosityFirst)(outputString(colorLuminosityFirst)),
+  );
 } else {
-  console.log(chalk.hex(color)(outputString(color)));
+  console.log(chalk.hex(colorHueFirst)(outputString(colorHueFirst)));
 }
